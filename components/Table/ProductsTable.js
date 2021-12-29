dd2178f462c04c2e4c42750e7fc0b8ddce1afb98
unknown <yleecruz@gmail.com>
2021-12-29 16:12:18 -0500

    header menu m

--- components/Table/ProductsTable.js
+++ components/Table/ProductsTable.js
@@ -5,69 +5,61 @@ export default function ProductsTable({
 }) {
   return (
     <div class="col-span-full xl:col-span-8 bg-white shadow-lg rounded-sm border border-gray-200 m-2">
-      <header class="px-5 py-4 border-b border-gray-100 flex flex-row justify-between">
+      <header class="px-5 py-4 border-b border-gray-100 flex flex-row justify-between items-center">
         <h2 class="font-semibold text-gray-800">Productos</h2>
-        <div className="flex flex-row text-center items-center">
-          <p>Adicionar Producto</p>
+        <div className="flex flex-row items-center ">
+          <p className='text-gray-700 text-sm'>Adicionar Producto</p>
           <i
             className="bg-gray-700 text-white p-1 px-2 m-2 rounded-lg las la-plus  "
             onClick={() => setShowModal(true)}
           ></i>
         </div>
       </header>
-      <div class="p-3">
-        <div class="overflow-x-auto ">
-          <table class="table-auto w-full">
-            <thead class="text-xs uppercase text-gray-400 bg-gray-50 rounded-sm">
+      <div className="p-3">
+        <div className="overflow-x-auto ">
+          <table className="table-auto w-full">
+            <thead className="text-xs uppercase text-gray-400 bg-gray-50 rounded-sm">
               <tr>
-                <th class="p-2">
+                <th className="p-2">
                   <div class="font-semibold text-left">Nombre</div>
                 </th>
-                <th class="p-2">
-                  <div class="font-semibold text-center">Ventas</div>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Ventas</div>
                 </th>
-                <th class="p-2">
-                  <div class="font-semibold text-center">Existencia</div>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Existencia</div>
                 </th>
-                <th class="p-2">
-                  <div class="font-semibold text-center">Vendidos</div>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Vendidos</div>
                 </th>
-                <th class="p-2">
-                  <div class="font-semibold text-center">Actions</div>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Actions</div>
                 </th>
               </tr>
             </thead>
-            <tbody class="text-sm font-medium divide-y divide-gray-100">
+            <tbody className="text-sm font-medium divide-y divide-gray-100 items-center">
               {products ? (
                 products.map((product) => (
                   <tr key={product.id}>
-                    <td class="p-2">
-                      <div class="flex items-center">
-                        <div class="text-gray-800">{product.name}</div>
-                      </div>
+                    <td className="p-2 flex items-center">
+                      <div className="text-gray-800">{product.name}</div>
                     </td>
 
-                    <td class="p-2">
-                      <div class="text-center text-green-500">
-                        ${product.quantity + product.salePrice}
-                      </div>
+                    <td className="p-2 text-center text-green-500">
+                      ${product.quantity + product.salePrice}
                     </td>
-                    <td class="p-2">
-                      <div class="text-center">{product.quantity}</div>
+                    <td className="p-2">
+                      <div className="text-center">{product.quantity}</div>
                     </td>
-                    <td class="p-2">
-                      <div class="text-center text-light-blue-500">
-                        {product.quantitySold}
-                      </div>
+                    <td className="p-2 text-center text-light-blue-500">
+                      {product.quantitySold}
                     </td>
-                    <td class="p-2">
-                      <div class="text-center text-light-blue-500">
-                        <i className="las la-edit w-12 h-12 text-green-500 text-xl cursor-pointer"></i>
-                        <i
-                          className="las la-trash w-12 h-12 text-red-500 text-xl cursor-pointer"
-                          onClick={() => handleDeleteProduct(product.id)}
-                        ></i>
-                      </div>
+                    <td className="p-2 text-center text-light-blue-500 flex flex-row">
+                      <i className="las la-edit w-12 h-12 text-green-500 text-xl cursor-pointer"></i>
+                      <i
+                        className="las la-trash w-12 h-12 text-red-500 text-xl cursor-pointer"
+                        onClick={() => handleDeleteProduct(product.id)}
+                      ></i>
                     </td>
                   </tr>
                 ))
