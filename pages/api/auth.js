9c60dc12210c9efdcc85699046ce1aee38f71693
unknown <yleecruz@gmail.com>
2022-01-08 15:01:45 -0500

    auth add routes

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,7 +2,6 @@ import { supabase } from "../../utils/supabaseClient";
 
 const handler = (req, res) => {
   supabase.auth.api.setAuthCookie(req, res);
-  console.log("API CALL")
 };
 
 export default handler;
