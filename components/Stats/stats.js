052bd98c2628c3e7863153d47e62ae75b21bf511
unknown <yleecruz@gmail.com>
2022-01-24 14:52:34 -0500

    added animated spin

--- components/Stats/stats.js
+++ components/Stats/stats.js
@@ -27,37 +27,8 @@ export default function Stats({ day }) {
   return (
     <>
       {loading ? (
-        <div className="grid grid-flow-col text-xs text-gray-400  rounded-lg gap-3 grid-cols-3 items-center  bg-gray-100 m-2 p-2 ">
-          <div className="relative flex flex-col rounded-lg shadow-md px-2 py-2 bg-white cursor-pointer text-center  focus:outline-none">
-            <p className="text-center ">Ganancia</p>
-            <p
-              className={`${
-                dailySales - dailyPurchases > 0
-                  ? "bg-green-100 text-green-800"
-                  : "text-red-500 bg-red-50"
-              } p-2 inline-flex text-lg mt-2 font-bold  rounded-lg `}
-            >
-              <span className="flex flex-col mx-auto">
-                <i class="las la-hourglass-start animate-bounce"></i>
-              </span>
-            </p>
-          </div>
-          <div className="relative flex flex-col rounded-lg shadow-md px-2 py-2 bg-white cursor-pointer text-center  focus:outline-none">
-            <p className="text-center ">Venta</p>
-            <p className="p-2 inline-flex  text-lg mt-2 font-bold rounded-lg bg-green-100 text-green-800">
-              <span className="flex flex-col mx-auto">
-                <i class="las la-hourglass-start animate-bounce"></i>
-              </span>
-            </p>
-          </div>
-          <div className="relative flex flex-col rounded-lg shadow-md px-2 py-2 bg-white cursor-pointer   focus:outline-none">
-            <p className="text-center ">Compra</p>
-            <p className="p-2 inline-flex text-lg mt-2 font-bold text-red-500 rounded-lg bg-red-50">
-              <span className="flex flex-col mx-auto">
-                <i class="las la-hourglass-start animate-bounce"></i>
-              </span>
-            </p>
-          </div>
+        <div className="text-gray-400 text-center h-20  rounded-lg  items-center  bg-gray-100 m-2 p-2 ">
+          <i class="las la-spinner animate-spin mt-2 text-gray-400 text-5xl text-center"></i>
         </div>
       ) : (
         <div className="grid grid-flow-col text-xs text-gray-400  rounded-lg gap-3 grid-cols-3 items-center  bg-gray-100 m-2 p-2 ">
